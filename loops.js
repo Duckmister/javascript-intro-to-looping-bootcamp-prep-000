278c5f248c0c04d138122adf8dd5475eefcce774
T Hinder <theresa.hindersinn@gmail.com>
2017-09-04 17:35:35 +0000

    Done.

--- loops.js
+++ loops.js
@@ -2,10 +2,10 @@
 
 function forLoop(array){
   for (var i=0; i<26; i++){
-    if(i=1){
+    if(i===1){
       array.push('I am 1 strange loop.');
     } else{
-      array.push('I am '${i}' strange loops.')
+      array.push('I am ' + ${i} + ' strange loops.')
     }
   }
   return array
@@ -15,7 +15,7 @@ var array = {}
 
 function forLoop(array){
   for (var i = 0; i < 26; i++){
-    if (i=1){
+    if (i===1){
     array.push("I am " ${i} " strange loop.");
     }
     else{
