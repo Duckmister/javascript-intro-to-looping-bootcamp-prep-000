88cee4bad8dd6b239e475a81379c3ee9f9f890f5
T <theresa.hindersinn@gmail.com>
2017-09-04 18:34:20 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,34 +1,11 @@
-var array = [];
 
 function forLoop(array){
-  for (let i = 0; i < 26; i++) {
+  for (let i = 0; i < 25; i++) {
     if(i === 1){
       array.push('I am 1 strange loop.');
     }
     else{
-      array.push('I am ${i}  strange loops.');
+      array.push('I am ' + ${i} + ' strange loops.');
     }
-  }
-}
-
-
-
-function forLoop(array){
-  for (let i = 0; i < 26; i++){
-    if (i===1){
-    console.log("I am " ${i} " strange loop.");
-    }
-    else{
-      console.log("I am " ${i} " strange loops.");
-    }
-  }
-}
-
-function whileLoop(n){
-  while (n>0){
-    return --n;
-  }
-  if (n=0){
-    return "done";
   }
 }
